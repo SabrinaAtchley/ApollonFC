103a7eb419ce247230d5eaa78d92a847658768f9
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-02-08 17:05:02 -0600

    Added Receivers unit test

--- unit-tests.h
+++ unit-tests.h
@@ -8,11 +8,24 @@
 
 #include "unit-tests/test-vector16.h"
 
+/* Flag(s) for which tests should be run (running all at once can run
+ * into program memory issues on low-memory boards such as the Arduino Uno)
+ * Options:
+ *  FLIGHT_CONTROLLER_UNIT_TEST_VECTOR16
+ *  FLIGHT_CONTROLLER_UNIT_TEST_RECEIVERS
+ */
+#define FLIGHT_CONTROLLER_UNIT_TEST_VECTOR16
+
 
 void setup() {
   Serial.begin(9600);
 
-  unitVector16();
+  #ifdef FLIGHT_CONTROLLER_UNIT_TEST_VECTOR16
+    unitVector16();
+  #endif
+  #ifdef FLIGHT_CONTROLLER_UNIT_TEST_RECEIVERS
+    unitReceivers();
+  #endif
 }
 
 void loop(){};
