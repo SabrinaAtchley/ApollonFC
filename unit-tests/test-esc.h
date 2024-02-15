777b133e315cd06a1fc9c32344387906c3a029a5
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-02-15 01:32:18 -0600

    Refactor using CALIBRATE and WRITE_ALL macros

--- unit-tests/test-esc.h
+++ unit-tests/test-esc.h
@@ -7,24 +7,25 @@
 void unitESC() {
   RECEIVER_T receiver;
   SpeedController::setup();
-  uint16_t channels[INPUT_CHANNELS];
   uint16_t signals[MOTOR_AMOUNT];
   bool isArmed = false;
 
-  #define IS_ARM_SWITCH_DOWN (channels[INPUT_CHANNEL_SWA - 1] == INPUT_MOTOR_MAX)
-  #define IS_THROTTLE_DOWN (channels[INPUT_CHANNEL_THROTTLE - 1] == INPUT_MOTOR_MIN)
+  #define IS_ARM_SWITCH_DOWN (receiver.getChannel(INPUT_CHANNEL_SWA) == INPUT_MOTOR_MAX)
+  #define IS_THROTTLE_DOWN (receiver.getChannel(INPUT_CHANNEL_THROTTLE) == INPUT_MOTOR_MIN)
 
+  SPEED_CONTROLLER_CALIBRATE(signals);
+
+  // Throttle response test
   while(true) {
-    receiver.getControls(channels);
-    for (uint8_t i = 0; i < MOTOR_AMOUNT; i++) {
-      isArmed = (isArmed && IS_ARM_SWITCH_DOWN) || (IS_ARM_SWITCH_DOWN && IS_THROTTLE_DOWN);
+    receiver.update();
 
-      if (isArmed)
-        signals[i] = channels[INPUT_CHANNEL_THROTTLE - 1];
-      else
-        signals[i] = 1000;
-    }
-    Serial.println(channels[INPUT_CHANNEL_THROTTLE - 1]);
+    isArmed = (isArmed && IS_ARM_SWITCH_DOWN) || (IS_ARM_SWITCH_DOWN && IS_THROTTLE_DOWN);
+    SPEED_CONTROLLER_WRITE_ALL(
+      signals,
+      isArmed ? receiver.getChannel(INPUT_CHANNEL_THROTTLE) : INPUT_MOTOR_MIN
+    );
+
+    Serial.println(receiver.getChannel(INPUT_CHANNEL_THROTTLE));
     SpeedController::write(signals);
   }
 } /* unitEsc */
