e7ce2ae66d33355ec32eb31840d5ceabd91e84ad
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-02-25 23:32:25 -0600

    Refactor, added types for configuration bytes

--- src/sensors/HMC5883L.h
+++ src/sensors/HMC5883L.h
@@ -12,32 +12,71 @@
 
 enum : byte {
   HMC5883L_ADDRESS = 0x1E,
-  HMC5883L_MODE = 0x01, // 1-bit mode: 0 continuous measurement, 1 takes a new measurement only after the old one was read
   HMC5883L_HIGH_SPEED = 0x00, // Set to 0x01 to use high speed I2C
-  HMC5883L_REGISTER_CFGA = 0X00,
-  HMC5883L_REGISTER_CFGB = 0X01,
-  HMC5883L_REGISTER_MODE = 0X02,
-  HMC5883L_REGISTER_X_MSB = 0X03,
-  HMC5883L_REGISTER_X_LSB = 0X04,
-  HMC5883L_REGISTER_Z_MSB = 0X05,
-  HMC5883L_REGISTER_Z_LSB = 0X06,
-  HMC5883L_REGISTER_Y_MSB = 0X07,
-  HMC5883L_REGISTER_Y_LSB = 0X08,
-  HMC5883L_REGISTER_STATUS = 0X09,
-  HMC5883L_REGISTER_IDA = 0X0A,
-  HMC5883L_REGISTER_IDB = 0X0B,
-  HMC5883L_REGISTER_IDC = 0X0C,
-  HMC5883L_REGISTER_TEMP_MSB = 0X31,
-  HMC5883L_REGISTER_TEMP_LSB = 0X32,
+  HMC5883L_REGISTER_CFGA = 0x00,
+  HMC5883L_REGISTER_CFGB = 0x01,
+  HMC5883L_REGISTER_MODE = 0x02,
+  HMC5883L_REGISTER_X_MSB = 0x03,
+  HMC5883L_REGISTER_X_LSB = 0x04,
+  HMC5883L_REGISTER_Z_MSB = 0x05,
+  HMC5883L_REGISTER_Z_LSB = 0x06,
+  HMC5883L_REGISTER_Y_MSB = 0x07,
+  HMC5883L_REGISTER_Y_LSB = 0x08,
+  HMC5883L_REGISTER_STATUS = 0x09,
+  HMC5883L_REGISTER_IDA = 0x0A,
+  HMC5883L_REGISTER_IDB = 0x0B,
+  HMC5883L_REGISTER_IDC = 0x0C,
+  HMC5883L_REGISTER_TEMP_MSB = 0x31,
+  HMC5883L_REGISTER_TEMP_LSB = 0x32,
 };
 
+typedef enum : byte { // 1-bit mode
+  HMC5883L_MODE_CONTINUOUS = 0b0,
+  HMC5883L_MODE_SINGLE = 0b1,
+} hmc5883l_mode_t;
+
+typedef enum : byte { // 3-bit gain value
+  HMC5883L_GAIN_0 = 0x0,
+  HMC5883L_GAIN_1 = 0x1,
+  HMC5883L_GAIN_2 = 0x2,
+  HMC5883L_GAIN_3 = 0x3,
+  HMC5883L_GAIN_4 = 0x4,
+  HMC5883L_GAIN_5 = 0x5,
+  HMC5883L_GAIN_6 = 0x6,
+  HMC5883L_GAIN_7 = 0x7,
+} hmc5883l_gain_t;
+
+typedef enum : byte { // 2-bits
+  HMC5883L_OSS_0 = 0x0,
+  HMC5883L_OSS_1 = 0x1,
+  HMC5883L_OSS_2 = 0x2,
+  HMC5883L_OSS_3 = 0x3,
+} hmc5883l_oss_t;
+
+typedef enum : byte { // 3-bits
+  HMC5883L_OUTRATE_0 = 0x0,
+  HMC5883L_OUTRATE_1 = 0x1,
+  HMC5883L_OUTRATE_2 = 0x2,
+  HMC5883L_OUTRATE_3 = 0x3,
+  HMC5883L_OUTRATE_4 = 0x4,
+  HMC5883L_OUTRATE_5 = 0x5,
+  HMC5883L_OUTRATE_6 = 0x6,
+  HMC5883L_OUTRATE_7 = 0x7,
+} hmc5883l_outrate_t;
+
+typedef enum : byte { // 2-bits, excluding 0b11
+  HMC5883L_BIAS_0 = 0x0,
+  HMC5883L_BIAS_1 = 0x1,
+  HMC5883L_BIAS_2 = 0x2,
+} hmc5883l_bias_t;
 
 
 class Sensor_HMC5883L : I2C_Sensor {
 private:
   byte lock; // Status bit 1, write locks output registers
   byte ready; // Status bit 0, high when measurements are ready
-  byte gain; // 3 bits, Valid values from 0x00 - 0x07. Higher gain # decreases actual gain and increases signal range
+  hmc5883l_gain_t gain; // Higher gain # decreases actual gain and increases signal range
+  const hmc5883l_mode_t mode;
   static constexpr int16_t gainLSB_Gauss[] = {1370, 1090, 820, 660, 440, 390, 330, 230};
 public:
   int16_t x = 0, y = 0, z = 0; // magnetometer values in microGauss
@@ -53,9 +92,15 @@ private:
   void startMeasurement(const bool closeConnection);
 
 public:
-  Sensor_HMC5883L(byte _gain = 0x01, byte oss = 0x00, byte outRate = 0x04, byte bias = 0x00);
+  Sensor_HMC5883L(
+    const hmc5883l_mode_t _mode = HMC5883L_MODE_SINGLE,
+    const hmc5883l_gain_t _gain = HMC5883L_GAIN_1,
+    const hmc5883l_oss_t oss = HMC5883L_OSS_0,
+    const hmc5883l_outrate_t outRate = HMC5883L_OUTRATE_4,
+    const hmc5883l_bias_t bias = HMC5883L_BIAS_0
+  );
 
-  void setGain(const byte g, const bool closeConnection);
+  void setGain(const hmc5883l_gain_t g, const bool closeConnection);
 
   bool update();
 };
@@ -66,11 +111,12 @@ constexpr int16_t Sensor_HMC5883L::gainLSB_Gauss[]; // Fixed undefined reference
 /* Implementation */
 
 Sensor_HMC5883L::Sensor_HMC5883L(
-    byte _gain = 0x01,
-    byte oss = 0x00,
-    byte outRate = 0x04,
-    byte bias = 0x00
-) : I2C_Sensor(HMC5883L_ADDRESS) {
+    const hmc5883l_mode_t _mode = HMC5883L_MODE_SINGLE,
+    const hmc5883l_gain_t _gain = HMC5883L_GAIN_1,
+    const hmc5883l_oss_t oss = HMC5883L_OSS_0,
+    const hmc5883l_outrate_t outRate = HMC5883L_OUTRATE_4,
+    const hmc5883l_bias_t bias = HMC5883L_BIAS_0
+) : I2C_Sensor(HMC5883L_ADDRESS), mode(_mode) {
   byte id[3];
 
   // Check ID = H43
@@ -78,11 +124,6 @@ Sensor_HMC5883L::Sensor_HMC5883L(
   readIntoArray(id, 3, connection.close);
   if (id[0] != 0x48 || id[1] != 0x34 || id[2] != 0x33)
     ERROR(3); // I2C configuration error
-
-  // Restrict configuration variables to correct bitsize
-  oss &= 0x03; // 2 bits
-  outRate &= 0x07; // 3 bits
-  bias &= 0x03; // 2 bits
 
   // Configure device
   setGain(_gain, connection.keepAlive);
@@ -99,38 +140,34 @@ void Sensor_HMC5883L::readStatus(const bool closeConnection) {
 }
 
 void Sensor_HMC5883L::startMeasurement(const bool closeConnection) {
-  write8(HMC5883L_REGISTER_MODE, (HMC5883L_HIGH_SPEED << 7) | HMC5883L_MODE, closeConnection);
+  write8(HMC5883L_REGISTER_MODE, (HMC5883L_HIGH_SPEED << 7) | mode, closeConnection);
 }
 
-void Sensor_HMC5883L::setGain(const byte g, const bool closeConnection) {
-  gain = min(g, 0x07);
+void Sensor_HMC5883L::setGain(const hmc5883l_gain_t g, const bool closeConnection) {
+  gain = g;
   write8(HMC5883L_REGISTER_CFGB, gain << 5, closeConnection);
 }
 
 bool Sensor_HMC5883L::update() {
-  int8_t data[6]; // stores x<15:0>, z<15:0>, y<15:0>
-  int16_t data_x, data_y, data_z;
+  int16_t data[3];
 
-  readStatus(connection.keepAlive);
+  readStatus(connection.close); // Close connection in case not ready
   if (ready) {
     // Read magnetometer data
     selectRegister(HMC5883L_REGISTER_X_MSB, connection.keepAlive);
-    readIntoArray(data, 6, connection.keepAlive);
-    startMeasurement(connection.close);
-    data_x = ((int16_t) data[0] << 8) | data[1];
-    data_z = ((int16_t) data[2] << 8) | data[3];
-    data_y = ((int16_t) data[4] << 8) | data[5];
+    readIntoArray(data, 3, connection.keepAlive);
+    startMeasurement(connection.close); // Start next measurement cycle
 
     // If any value is -4096, it has overflowed
-    if (data_x == -4096 || data_y == -4096 || data_z == -4096) {
+    if (data[0] == -4096 || data[1] == -4096 || data[2] == -4096) {
       Serial.println("WARNING HMC5883L: integer overflow detected (magnetic field outside sensor range). Try a higher gain value if this problem persists.");
       return false;
     }
 
-    // Convert from count (LSB) to Gauss measurements
-    x = ( (int32_t) data_x * 1000) / gainLSB_Gauss[gain];
-    z = ( (int32_t) data_z * 1000) / gainLSB_Gauss[gain];
-    y = ( (int32_t) data_y * 1000) / gainLSB_Gauss[gain];
+    // Convert from count (LSB) to microGauss measurements
+    x = ( (int32_t) data[0] * 1000) / gainLSB_Gauss[gain];
+    z = ( (int32_t) data[1] * 1000) / gainLSB_Gauss[gain];
+    y = ( (int32_t) data[2] * 1000) / gainLSB_Gauss[gain];
 
     return true;
   }
