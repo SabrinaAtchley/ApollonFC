1fb9b05111eaafa6e06c003f38abf614b082722b
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-02-25 23:31:55 -0600

    Refactored readIntoArray to work for multi-byte types

--- src/sensor.h
+++ src/sensor.h
@@ -64,9 +64,13 @@ protected:
 
   template<typename T>
   void readIntoArray(T *arr, const uint8_t n, const bool closeConnection) {
-    Wire.requestFrom(address, (byte) n);
-    for (uint8_t i = 0; i < n; i++)
-      arr[i] = (T) Wire.read();
+    Wire.requestFrom(address, (byte) n * sizeof(T));
+    for (uint8_t i = 0; i < n; i++) {
+      arr[i] = 0;
+      for (uint8_t j = sizeof(T); j > 0; j--) {
+        arr[i] |= (T) Wire.read() << (8 * (j - 1));
+      }
+    }
     Wire.endTransmission(closeConnection);
   }
 };
