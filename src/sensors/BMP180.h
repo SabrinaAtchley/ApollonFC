6e9df90f2b4e6741907d0e2ff0747461ce9de96e
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-02-25 22:43:21 -0600

    Refactor, separated implementation, updated I2C interface

--- src/sensors/BMP180.h
+++ src/sensors/BMP180.h
@@ -56,9 +56,11 @@ typedef enum : byte {
  */
 class Sensor_BMP180 : I2C_Sensor {
 private:
-  int16_t ac1, ac2, ac3, b1, b2, mb, mc, md; // Calibration data
-  uint16_t ac4, ac5, ac6; // Calibration data
+  // Calibration data
+  int16_t ac1, ac2, ac3, b1, b2, mb, mc, md;
+  uint16_t ac4, ac5, ac6;
   int32_t b5 = 0;
+
   const uint32_t seaLevel;
   bmp180_mode_t mode; // Over sampling mode for pressure measurements (OSS)
   bmp180_conversion_step_t step = BMP180_STEP_GET_UT;
@@ -70,139 +72,178 @@ public:
   float altitude = 0; // Altitude in meters
 
 private:
-  void readCalibrationData() {
-    read16(BMP180_REGISTER_AC1, ac1);
-    read16(BMP180_REGISTER_AC2, ac2);
-    read16(BMP180_REGISTER_AC3, ac3);
-    read16(BMP180_REGISTER_AC4, ac4);
-    read16(BMP180_REGISTER_AC5, ac5);
-    read16(BMP180_REGISTER_AC6, ac6);
-    read16(BMP180_REGISTER_B1, b1);
-    read16(BMP180_REGISTER_B2, b2);
-    read16(BMP180_REGISTER_MB, mb);
-    read16(BMP180_REGISTER_MC, mc);
-    read16(BMP180_REGISTER_MD, md);
-  }
+  /* Reads calibration data and sets calibration variables
+   * ac1, ac2, ac3, ac4, ac5, ac6, b1, b2, mb, mc, md
+   */
+  void readCalibrationData(const bool closeConnection);
 
   // reads SCO bit to see if conversion is finished
-  bool readSCO() {
-    byte ctrl_meas;
-    read8(BMP180_REGISTER_CTRL_MEAS, ctrl_meas);
-    return 0x20 & ctrl_meas; // return bit 5, sco
-  }
+  bool readSCO(const bool closeConnection);
 
   // param: ut, calibration data
-  void computeB5() {
-    int32_t x1 = ((ut - (int32_t) ac6) * (int32_t) ac5) >> 15;
-    int32_t x2 = ((int32_t) mc << 11) / (x1 + (int32_t) md);
-    b5 = x1 + x2;
-  }
+  void computeB5();
 
   // Begins temperature conversion
-  void requestTemperature() {
-    write8(BMP180_REGISTER_CTRL_MEAS, BMP180_CMD_TEMPERATURE);
-  }
+  void requestTemperature(const bool closeConnection);
 
   // Begins pressure conversion
-  void requestPressure() {
-    write8(BMP180_REGISTER_CTRL_MEAS, BMP180_CMD_PRESSURE + (mode << 6));
-  }
+  void requestPressure(const bool closeConnection);
 
-  void getRawTemperature() {
-    read16(BMP180_REGISTER_OUT_MSB, ut);
-  }
+  // Reads raw temperature (assumes ready)
+  void getRawTemperature(const bool closeConnection);
 
-  void getRawPressure() {
-    uint16_t msb;
-    uint8_t xlsb;
-    read16(BMP180_REGISTER_OUT_MSB, msb);
-    read8(BMP180_REGISTER_OUT_XLSB, xlsb);
-    up = (((uint32_t) msb << 8) + xlsb) >> (8 - mode);
-  }
+  // Reads raw pressure (assumes ready)
+  void getRawPressure(const bool closeConnection);
 
-  void compensateTemperature() {
-    temperature = (b5 + 8) >> 4; // temperature in 0.1°C
-  }
+  // Compensates UT reading
+  void compensateTemperature();
 
   // Param: ut, up, calibration data
-  void compensatePressure() {
-    int32_t x1, x2, b6, x3, b3, p;
-    uint32_t b4, b7;
+  void compensatePressure();
 
-    /* Check datasheet for compensation algorithm */
-    b6 = b5 - 4000;
-    x1 = (b2 * ((b6 * b6) >> 12)) >> 11;
-    x2 = (ac2 * b6) >> 11;
-    x3 = x1 + x2;
-    b3 = ((( (int32_t) ac1 * 4 + x3) << mode) + 2) >> 2;
-    x1 = (ac3 * b6) >> 13;
-    x2 = (b1 * ((b6 * b6) >> 12)) >> 16;
-    x3 = ((x1 + x2) + 2) >> 2;
-    b4 = (ac4 * (uint32_t) (x3 + 32768)) >> 15;
-    b7 = (uint32_t) (up - b3) * (50000 >> mode);
-
-    if (b7 < 0x80000000) {
-      p = (b7 << 1) / b4;
-    } else {
-      p = (b7 / b4) << 1;
-    }
-
-    x1 = (p >> 8) * (p >> 8);
-    x1 = (x1 * 3038) >> 16;
-    x2 = (-7357 * p) >> 16;
-    pressure = p + ((x1 + x2 + 3791) >> 4);
-  }
-
-  void pressureToAltitude() {
-    // Uses international barometric formula found in the BMP180 datasheet
-    altitude = 44330.0 * (1.0 - pow((float) pressure / seaLevel, 0.19029));
-  }
+  // Estimates altitude from UP and sea level using International Barometric formula
+  void pressureToAltitude();
 
 public:
-  Sensor_BMP180(const bmp180_mode_t m, const uint32_t _seaLevel)
-      : I2C_Sensor(BMP180_ADDRESS), mode(m), seaLevel(_seaLevel)
-    {
-    uint8_t id;
-    read8(BMP180_REGISTER_CHIPID, id);
-    if (id != BMP180_CHIP_ID)
-      ERROR(3); // I2C device misconfiguration error
-
-    readCalibrationData();
-  }
+  Sensor_BMP180(const bmp180_mode_t m, const uint32_t _seaLevel);
 
   /* Called once per loop to update sensor values */
-  bool update() {
-    switch(step) {
-      case BMP180_STEP_GET_UT:
-        if (readSCO())
-          break; // Still converting
-
-        // UT is finished converting
-        getRawTemperature();
-        requestPressure();
-        step = BMP180_STEP_GET_UP;
-        break;
-
-      case BMP180_STEP_GET_UP:
-        if (readSCO())
-          break; // Still converting
-
-        // UP is finished converting
-        getRawPressure();
-        requestTemperature();
-        step = BMP180_STEP_COMPENSATE;
-
-      case BMP180_STEP_COMPENSATE:
-        computeB5();
-        compensateTemperature();
-        compensatePressure();
-        pressureToAltitude();
-        step = BMP180_STEP_GET_UT;
-        return true; // Signal to FC that new data is available
-    }
-    return false;
-  }
+  bool update();
 };
 
+
+
+/* Implementation */
+
+void Sensor_BMP180::readCalibrationData(const bool closeConnection) {
+  selectRegister(BMP180_REGISTER_AC1, connection.keepAlive);
+  read16(ac1, connection.keepAlive);
+  read16(ac2, connection.keepAlive);
+  read16(ac3, connection.keepAlive);
+  read16(ac4, connection.keepAlive);
+  read16(ac5, connection.keepAlive);
+  read16(ac6, connection.keepAlive);
+  read16(b1, connection.keepAlive);
+  read16(b2, connection.keepAlive);
+  read16(mb, connection.keepAlive);
+  read16(mc, connection.keepAlive);
+  read16(md, closeConnection);
+}
+
+bool Sensor_BMP180::readSCO(const bool closeConnection) {
+  byte ctrl_meas;
+  selectRegister(BMP180_REGISTER_CTRL_MEAS, connection.keepAlive);
+  read8(ctrl_meas, closeConnection);
+  return 0x20 & ctrl_meas; // return bit 5, sco
+}
+
+void Sensor_BMP180::computeB5() {
+  int32_t x1 = ((ut - (int32_t) ac6) * (int32_t) ac5) >> 15;
+  int32_t x2 = ((int32_t) mc << 11) / (x1 + (int32_t) md);
+  b5 = x1 + x2;
+}
+
+void Sensor_BMP180::requestTemperature(const bool closeConnection) {
+  write8(BMP180_REGISTER_CTRL_MEAS, BMP180_CMD_TEMPERATURE, closeConnection);
+}
+
+void Sensor_BMP180::requestPressure(const bool closeConnection) {
+  write8(BMP180_REGISTER_CTRL_MEAS, BMP180_CMD_PRESSURE + (mode << 6), closeConnection);
+}
+
+void Sensor_BMP180::getRawTemperature(const bool closeConnection) {
+  selectRegister(BMP180_REGISTER_OUT_MSB, connection.keepAlive);
+  read16(ut, closeConnection);
+}
+
+void Sensor_BMP180::getRawPressure(const bool closeConnection) {
+  uint16_t msb;
+  uint8_t xlsb;
+  selectRegister(BMP180_REGISTER_OUT_MSB, connection.keepAlive);
+  read16(msb, connection.keepAlive);
+  read8(xlsb, closeConnection);
+  up = (((uint32_t) msb << 8) + xlsb) >> (8 - mode);
+}
+
+void Sensor_BMP180::compensateTemperature() {
+  temperature = (b5 + 8) >> 4; // temperature in 0.1°C
+}
+
+void Sensor_BMP180::compensatePressure() {
+  int32_t x1, x2, b6, x3, b3, p;
+  uint32_t b4, b7;
+
+  /* Check datasheet for compensation algorithm */
+  b6 = b5 - 4000;
+  x1 = (b2 * ((b6 * b6) >> 12)) >> 11;
+  x2 = (ac2 * b6) >> 11;
+  x3 = x1 + x2;
+  b3 = ((( (int32_t) ac1 * 4 + x3) << mode) + 2) >> 2;
+  x1 = (ac3 * b6) >> 13;
+  x2 = (b1 * ((b6 * b6) >> 12)) >> 16;
+  x3 = ((x1 + x2) + 2) >> 2;
+  b4 = (ac4 * (uint32_t) (x3 + 32768)) >> 15;
+  b7 = (uint32_t) (up - b3) * (50000 >> mode);
+
+  if (b7 < 0x80000000) {
+    p = (b7 << 1) / b4;
+  } else {
+    p = (b7 / b4) << 1;
+  }
+
+  x1 = (p >> 8) * (p >> 8);
+  x1 = (x1 * 3038) >> 16;
+  x2 = (-7357 * p) >> 16;
+  pressure = p + ((x1 + x2 + 3791) >> 4);
+}
+
+void Sensor_BMP180::pressureToAltitude() {
+  // Uses international barometric formula found in the BMP180 datasheet
+  altitude = 44330.0 * (1.0 - pow((float) pressure / seaLevel, 0.19029));
+}
+
+Sensor_BMP180::Sensor_BMP180(const bmp180_mode_t m, const uint32_t _seaLevel)
+    : I2C_Sensor(BMP180_ADDRESS), mode(m), seaLevel(_seaLevel)
+{
+  uint8_t id;
+  selectRegister(BMP180_REGISTER_CHIPID, connection.keepAlive);
+  read8(id, connection.keepAlive);
+  if (id != BMP180_CHIP_ID)
+    ERROR(3); // I2C device misconfiguration error
+
+  readCalibrationData(connection.close);
+}
+
+bool Sensor_BMP180::update() {
+  switch(step) {
+    case BMP180_STEP_GET_UT:
+      if (readSCO(connection.close)) // Close connection in case of break
+        break; // Still converting
+
+      // UT is finished converting
+      getRawTemperature(connection.keepAlive);
+      requestPressure(connection.close);
+      step = BMP180_STEP_GET_UP;
+      break;
+
+    case BMP180_STEP_GET_UP:
+      if (readSCO(connection.close)) // Close connection in case of break
+        break; // Still converting
+
+      // UP is finished converting
+      getRawPressure(connection.keepAlive);
+      requestTemperature(connection.close); // Start measurements for next cycle
+      step = BMP180_STEP_COMPENSATE;
+
+    case BMP180_STEP_COMPENSATE:
+      computeB5();
+      compensateTemperature();
+      compensatePressure();
+      pressureToAltitude();
+      step = BMP180_STEP_GET_UT;
+      return true; // Signal to FC that new data is available
+  }
+  return false;
+}
 
 #endif
--- src/sensors/HMC5883L.h
+++ src/sensors/HMC5883L.h
@@ -24,6 +24,8 @@ enum : byte {
   HMC5883L_REGISTER_TEMP_LSB = 0X32,
 };
 
+
+
 class Sensor_HMC5883L : I2C_Sensor {
 private:
   byte lock; // Status bit 1, write locks output registers
@@ -34,81 +36,98 @@ public:
   int16_t x = 0, y = 0, z = 0; // magnetometer values in microGauss
 
 private:
-  void readStatus() {
-    byte status;
-    read8(HMC5883L_REGISTER_STATUS, status);
-    lock = status & 0x02;
-    ready = status & 0x01;
-  }
+  /* Read status register for lock and ready bits */
+  void readStatus(const bool closeConnection);
 
   /* Sets mode register, which begins measurement
    * For continuous-mode operation, this only needs to be called once
    * For single-measurement operation, this needs to be called after data is read
    */
-  void startMeasurement() {
-    write8(HMC5883L_REGISTER_MODE, (HMC5883L_HIGH_SPEED << 7) | HMC5883L_MODE);
-  }
+  void startMeasurement(const bool closeConnection);
 
 public:
-  Sensor_HMC5883L(
-      byte _gain = 0x01,
-      byte oss = 0x00,
-      byte outRate = 0x04,
-      byte bias = 0x00
-    ) : I2C_Sensor(HMC5883L_ADDRESS)
-  {
-    // Check ID
-    byte id[3];
-    readIntoArray(HMC5883L_REGISTER_IDA, id, 3);
-    if (id[0] != 0x48 || id[1] != 0x34 || id[2] != 0x33)
-      ERROR(3); // I2C configuration error
+  Sensor_HMC5883L(byte _gain = 0x01, byte oss = 0x00, byte outRate = 0x04, byte bias = 0x00);
 
-    // Restrict configuration variables to correct bitsize
-    oss &= 0x03; // 2 bits
-    outRate &= 0x07; // 3 bits
-    bias &= 0x03; // 2 bits
+  void setGain(const byte g, const bool closeConnection);
 
-    // Configure device
-    setGain(_gain);
-    write8(HMC5883L_REGISTER_CFGA, (oss << 5) | (outRate << 3) | bias);
-    startMeasurement();
-  }
-
-  bool update() {
-    int8_t data[6]; // stores x<15:0>, z<15:0>, y<15:0>
-    int16_t data_x, data_y, data_z;
-
-    readStatus();
-    if (ready) {
-      // Read magnetometer data
-      readIntoArray(HMC5883L_REGISTER_X_MSB, data, 6), false;
-      startMeasurement();
-      data_x = ((int16_t) data[0] << 8) | data[1];
-      data_z = ((int16_t) data[2] << 8) | data[3];
-      data_y = ((int16_t) data[4] << 8) | data[5];
-
-      // If any value is -4096, it has overflowed
-      if (data_x == -4096 || data_y == -4096 || data_z == -4096) {
-        Serial.println("WARNING HMC5883L: integer overflow detected (magnetic field outside sensor range). Try a higher gain value if this problem persists.");
-        return false;
-      }
-
-      // Convert from count (LSB) to Gauss measurements
-      x = ( (int32_t) data_x * 1000) / gainLSB_Gauss[gain];
-      z = ( (int32_t) data_z * 1000) / gainLSB_Gauss[gain];
-      y = ( (int32_t) data_y * 1000) / gainLSB_Gauss[gain];
-
-      return true;
-    }
-    return false;
-  }
-
-  void setGain(const byte g) {
-    gain = min(g, 0x07);
-    write8(HMC5883L_REGISTER_CFGB, gain << 5);
-  }
+  bool update();
 };
-
 constexpr int16_t Sensor_HMC5883L::gainLSB_Gauss[]; // Fixed undefined reference error
+
+
+
+/* Implementation */
+
+Sensor_HMC5883L::Sensor_HMC5883L(
+    byte _gain = 0x01,
+    byte oss = 0x00,
+    byte outRate = 0x04,
+    byte bias = 0x00
+) : I2C_Sensor(HMC5883L_ADDRESS) {
+  byte id[3];
+
+  // Check ID = H43
+  selectRegister(HMC5883L_REGISTER_IDA, connection.keepAlive);
+  readIntoArray(id, 3, connection.close);
+  if (id[0] != 0x48 || id[1] != 0x34 || id[2] != 0x33)
+    ERROR(3); // I2C configuration error
+
+  // Restrict configuration variables to correct bitsize
+  oss &= 0x03; // 2 bits
+  outRate &= 0x07; // 3 bits
+  bias &= 0x03; // 2 bits
+
+  // Configure device
+  setGain(_gain, connection.keepAlive);
+  write8(HMC5883L_REGISTER_CFGA, (oss << 5) | (outRate << 3) | bias, connection.keepAlive);
+  startMeasurement(connection.close);
+}
+
+void Sensor_HMC5883L::readStatus(const bool closeConnection) {
+  byte status;
+  selectRegister(HMC5883L_REGISTER_STATUS, connection.keepAlive);
+  read8(status, closeConnection);
+  lock = status & 0x02;
+  ready = status & 0x01;
+}
+
+void Sensor_HMC5883L::startMeasurement(const bool closeConnection) {
+  write8(HMC5883L_REGISTER_MODE, (HMC5883L_HIGH_SPEED << 7) | HMC5883L_MODE, closeConnection);
+}
+
+void Sensor_HMC5883L::setGain(const byte g, const bool closeConnection) {
+  gain = min(g, 0x07);
+  write8(HMC5883L_REGISTER_CFGB, gain << 5, closeConnection);
+}
+
+bool Sensor_HMC5883L::update() {
+  int8_t data[6]; // stores x<15:0>, z<15:0>, y<15:0>
+  int16_t data_x, data_y, data_z;
+
+  readStatus(connection.keepAlive);
+  if (ready) {
+    // Read magnetometer data
+    selectRegister(HMC5883L_REGISTER_X_MSB, connection.keepAlive);
+    readIntoArray(data, 6, connection.keepAlive);
+    startMeasurement(connection.close);
+    data_x = ((int16_t) data[0] << 8) | data[1];
+    data_z = ((int16_t) data[2] << 8) | data[3];
+    data_y = ((int16_t) data[4] << 8) | data[5];
+
+    // If any value is -4096, it has overflowed
+    if (data_x == -4096 || data_y == -4096 || data_z == -4096) {
+      Serial.println("WARNING HMC5883L: integer overflow detected (magnetic field outside sensor range). Try a higher gain value if this problem persists.");
+      return false;
+    }
+
+    // Convert from count (LSB) to Gauss measurements
+    x = ( (int32_t) data_x * 1000) / gainLSB_Gauss[gain];
+    z = ( (int32_t) data_z * 1000) / gainLSB_Gauss[gain];
+    y = ( (int32_t) data_y * 1000) / gainLSB_Gauss[gain];
+
+    return true;
+  }
+  return false;
+}
 
 #endif
