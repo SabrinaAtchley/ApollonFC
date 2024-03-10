3642db81dd8fe28b0b5bf369bfade7b45d3a4985
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-03-10 15:13:38 -0500

    Split header

--- src/sensor.h
+++ src/sensor.h
@@ -1,7 +1,7 @@
 #ifndef APOLLON_FC_SRC_SENSOR_H
 #define APOLLON_FC_SRC_SENSOR_H
 
-#include <Wire.h>
+#include <Arduino.h>
 
 struct Connection {
   const bool keepAlive;
@@ -28,51 +28,35 @@ private:
   const byte address; // Device I2C address
 
 protected:
+  /* Allows connection.keepAlive and connection.close to be used
+   * for closing I2C connections or keeping them alive between reads/writes
+   *
+   * e.g. read8(BMP180_REGISTER_OUT_MSB, data, connection.keepAlive);
+   */
   static constexpr Connection connection = {false, true};
 
-  I2C_Sensor(const byte addr) : address(addr) {
-    // Enable I2C
-    Wire.begin();
-  };
+  // Initialize I2C with device address
+  I2C_Sensor(const byte addr);
 
-  void selectRegister(const byte reg, const bool closeConnection) {
-    Wire.beginTransmission(address);
-    Wire.write(reg);
-    Wire.endTransmission(closeConnection);
-  }
+  // Send register to slave before r/w
+  void selectRegister(const byte reg, const bool closeConnection);
 
-  void write8(const byte reg, const byte value, const bool closeConnection) {
-    Wire.beginTransmission(address);
-    Wire.write(reg);
-    Wire.write(value);
-    Wire.endTransmission(closeConnection);
-  }
+  // Write one byte to the given register
+  void write8(const byte reg, const byte value, const bool closeConnection);
 
+  // Read 1 byte from the current register and store in value
   template<typename T>
-  void read8(T &value, const bool closeConnection) {
-    Wire.requestFrom(address, (byte) 1);
-    value = (T) Wire.read();
-    Wire.endTransmission(closeConnection);
-  }
+  void read8(T &value, const bool closeConnection);
 
+  // Read 2 bytes from the current register and store in value
   template<typename T>
-  void read16(T &value, const bool closeConnection) {
-    Wire.requestFrom(address, (byte) 2);
-    value = (T) (Wire.read() << 8 | Wire.read());
-    Wire.endTransmission(closeConnection);
-  }
+  void read16(T &value, const bool closeConnection);
 
+  // Read n * sizeof(T) bytes from the current register and store in *arr
   template<typename T>
-  void readIntoArray(T *arr, const uint8_t n, const bool closeConnection) {
-    Wire.requestFrom(address, (byte) n * sizeof(T));
-    for (uint8_t i = 0; i < n; i++) {
-      arr[i] = 0;
-      for (uint8_t j = sizeof(T); j > 0; j--) {
-        arr[i] |= (T) Wire.read() << (8 * (j - 1));
-      }
-    }
-    Wire.endTransmission(closeConnection);
-  }
+  void readIntoArray(T *arr, const uint8_t n, const bool closeConnection);
 };
+
+#include "sensor.tpp"
 
 #endif /* APOLLON_FC_SRC_SENSORS_H */
