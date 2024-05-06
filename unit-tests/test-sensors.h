ea1df52046111e51eefa3134f987e741be6b98e1
SabrinaAtchley <Sabrina.Atchley0@gmail.com>
2024-05-05 20:21:58 -0500

    Added MPU6050 unit test

--- unit-tests/test-sensors.h
+++ unit-tests/test-sensors.h
@@ -3,14 +3,54 @@
 
 #include "../src/sensors/BMP180.h"
 #include "../src/sensors/HMC5883L.h"
+#include "../src/sensors/MPU6050.h"
 
 /* Tests various sensor libraries
  * Options:
  * APOLLON_FC_UNIT_TESTS_TEST_SENSORS_BMP180
  * APOLLON_FC_UNIT_TESTS_TEST_SENSORS_HMC5883L
+ * APOLLON_FC_UNIT_TESTS_TEST_SENSORS_MPU6050
  */
 
-#define APOLLON_FC_UNIT_TESTS_TEST_SENSORS_HMC5883L
+#define APOLLON_FC_UNIT_TESTS_TEST_SENSORS_MPU6050
+
+void test_mpu6050() {
+  unsigned long t = millis(); // time of last update
+  Sensor_MPU6050 mpu6050(false, false, false, MPU6050_GYRO_SCALE_500, MPU6050_ACCEL_SCALE_4G);
+
+  Serial.println("MPU6050");
+  Serial.println("Initiating self-test");
+  byte selfTestResult = mpu6050.selfTest();
+
+  if (selfTestResult ^ 0x3F) { // test failed
+    Serial.print("Self-test failed, result: ");
+    Serial.println(selfTestResult, BIN);
+    return;
+  }
+  Serial.println("Self test passed");
+
+  while (true) {
+    if (mpu6050.update()) {
+      Serial.print("Δt:");
+      Serial.print(millis() - t);
+      t = millis();
+      Serial.print(",accel_x:");
+      Serial.print(mpu6050.accel.x / 1000.0);
+      Serial.print(",accel_y:");
+      Serial.print(mpu6050.accel.y / 1000.0);
+      Serial.print(",accel_z:");
+      Serial.print(mpu6050.accel.z / 1000.0);
+      Serial.print(",gyro_x:");
+      Serial.print(mpu6050.gyro.x / 1000.0);
+      Serial.print(",gyro_y:");
+      Serial.print(mpu6050.gyro.y / 1000.0);
+      Serial.print(",gyro_z:");
+      Serial.print(mpu6050.gyro.z / 1000.0);
+      Serial.print(",temp:");
+      Serial.println(mpu6050.temp / 10.0);
+    }
+  }
+}
 
 void test_hmc5883l() {
   unsigned long t = millis(); // time of last update
@@ -18,7 +58,7 @@ void test_hmc5883l() {
   float heading;
 
   Serial.println("HMC5883L");
-  while(true) {
+  while (true) {
     if (hmc5883l.update()) { // If data is available
       Serial.print("Δt:");
       Serial.print(millis() - t);
@@ -42,7 +82,7 @@ void test_bmp180() {
   Sensor_BMP180 bmp180(BMP180_MODE_ULTRA_HIGH_RES, 102070);
 
   Serial.println("BMP180");
-  while(true) {
+  while (true) {
     if (bmp180.update()) { // If data is available
       Serial.print("Δt:");
       Serial.print(millis() - t);
@@ -67,6 +107,9 @@ void unitSensors() {
   #endif
   #ifdef APOLLON_FC_UNIT_TESTS_TEST_SENSORS_HMC5883L
     test_hmc5883l();
+  #endif
+  #ifdef APOLLON_FC_UNIT_TESTS_TEST_SENSORS_MPU6050
+    test_mpu6050();
   #endif
 }
 
