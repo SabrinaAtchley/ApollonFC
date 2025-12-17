c4ba7ae19263f2ddd25262dd694edbaea1206fde
Sabrina Atchley <Sabrina.Atchley0@gmail.com>
2025-12-17 15:59:22 -0600

    Added conversion functions and pgm_read_q16x16

--- src/q16x16/q16x16.h
+++ src/q16x16/q16x16.h
@@ -25,7 +25,17 @@ static constexpr Q16x16 ftoq16x16(const float x) {
   return (int32_t) (x * 65536);
 }
 
+static constexpr Q16x16 itoq16x16(const float x) {
+  return (Q16x16) ((int32_t) x << 16);
+}
 
+static constexpr float q16x16tof(const Q16x16 x) {
+  return ((float) x) / 65536.0;
+}
+
+static inline Q16x16 pgm_read_q16x16(const Q16x16 *p) {
+  return (Q16x16) pgm_read_dword(p);
+}
 
 
 #endif /* APOLLON_FC_SRC_Q16X16_H */
